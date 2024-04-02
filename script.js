52038b682e4db22292d509d920ea737298d3522a
Akshat-Sh <103410083+akshatshukla13@users.noreply.github.com>
2024-04-02 19:58:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const main = document.querySelector(".main");
+const submit = document.querySelector(".submit");
+const input = document.querySelector(".input");
+
+const newTask = document.createElement("div");
+const btn = document.createElement("input");
+const task = document.createElement("h2");
+const dlt = document.createElement("button");
+
+btn.type = "checkbox";
+dlt.type = "submit";
+
+dlt.innerText = "delete";
+task.style.display = "inline";
+btn.style.margin = "20px";
+task.style.marginRight = "20px";
+newTask.appendChild(dlt);
+newTask.appendChild(btn);
+newTask.appendChild(task);
+dlt.setAttribute("class", "dlt");
+btn.setAttribute("class", "btn");
+
+submit.addEventListener("click", (e) => {
+  task.innerHTML = input.value;
+  const p = newTask.cloneNode(true);
+  main.appendChild(p);
+  console.log(p);
+});
+
+
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('dlt')) {
+      e.target.parentNode.remove();
+    }
+  });
+
+
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('btn')) {
+        if (e.target.checked == true){
+            e.target.parentNode.children[2].style.textDecoration = "line-through";
+          } else {
+            e.target.parentNode.children[2].style.textDecoration = "";
+          }
+    }
+  });
